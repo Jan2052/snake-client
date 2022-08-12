8a932e2f754c4b7c9b5a14b2fe66eefc36b620a6
Janet <Janet_Lam@live.ca>
2022-08-12 02:50:09 -0400

    Moved over client input functions

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
 // const net = require("net");
 const connect = require("./client")
+const {setupInput, handleUserInput} = require("./input")
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function () {
-  setTimeout(() => {
-    process.stdin.on('data', (key) =>{
-      if (key === '\u0003') {
-        process.exit();
-      }
-    })
-  })
-};
 
 console.log("Connecting ...");
 connect();
